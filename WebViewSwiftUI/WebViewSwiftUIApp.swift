bccb21f5ac15a6e848e81a97bc84467a0094f2a8
Emre Dogan <emre.dogan@shortcut.no>
2023-01-20 13:49:18 +0100

    Initial Commit

---
+++ WebViewSwiftUI/WebViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewSwiftUIApp.swift
+//  WebViewSwiftUI
+//
+//  Created by Emre Dogan on 20/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct WebViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
